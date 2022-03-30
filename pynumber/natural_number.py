d102ec496ec0e8fe54827a467fc4f3a34b56e7d1
kkinugasa <kimiakikinugasa@gmail.com>
2022-03-31 04:39:05 +0900

    refactor: add type hints

--- pynumber/natural_number.py
+++ pynumber/natural_number.py
@@ -10,7 +10,7 @@ class NaturalNumber(tuple):
     """
 
     @property
-    def successor(self) -> NaturalNumber:
+    def successor(self: NaturalNumber) -> NaturalNumber:
         """Get a successor number
 
         Returns:
@@ -19,7 +19,7 @@ class NaturalNumber(tuple):
         return NaturalNumber((*tuple(self), tuple(self)))
 
     @property
-    def predecessor(self) -> NaturalNumber:
+    def predecessor(self: NaturalNumber) -> NaturalNumber:
         """Get a predecessor number
 
         Returns:
@@ -30,7 +30,7 @@ class NaturalNumber(tuple):
             raise ValueError("ZERO does not have a predecessor")
         return NaturalNumber(self[0:-1])
 
-    def __add__(self, obj: object) -> NaturalNumber:
+    def __add__(self: NaturalNumber, obj: object) -> NaturalNumber:
         if not isinstance(obj, NaturalNumber):
             raise TypeError(f"{obj} is not 'NaturalNumber'")
         # if obj == zero
@@ -39,7 +39,7 @@ class NaturalNumber(tuple):
         # m + n = (m + 1) + (n - 1)
         return self.successor + obj.predecessor
 
-    def __mul__(self, obj: object) -> NaturalNumber:
+    def __mul__(self: NaturalNumber, obj: object) -> NaturalNumber:
         if not isinstance(obj, NaturalNumber):
             raise TypeError(f"{obj} is not 'NaturalNumber'")
         # if obj == zero
@@ -48,26 +48,26 @@ class NaturalNumber(tuple):
         # m * n = m * (n - 1) + m
         return self * obj.predecessor + self
 
-    def __eq__(self, obj: object) -> bool:
+    def __eq__(self: NaturalNumber, obj: object) -> bool:
         return isinstance(obj, NaturalNumber) and tuple(self) == tuple(obj)
 
-    def __ne__(self, obj: object) -> bool:
+    def __ne__(self: NaturalNumber, obj: object) -> bool:
         return not self == obj
 
-    def __lt__(self, obj: object) -> bool:
+    def __lt__(self: NaturalNumber, obj: object) -> bool:
         if not isinstance(obj, NaturalNumber):
             raise TypeError(f"{obj} is not 'NaturalNumber'")
         return tuple(self) in tuple(obj)
 
-    def __gt__(self, obj: object) -> bool:
+    def __gt__(self: NaturalNumber, obj: object) -> bool:
         if not isinstance(obj, NaturalNumber):
             raise TypeError(f"{obj} is not 'NaturalNumber'")
         return tuple(obj) in tuple(self)
 
-    def __ge__(self, obj: object) -> bool:
+    def __ge__(self: NaturalNumber, obj: object) -> bool:
         return not self < obj
 
-    def __le__(self, obj: object) -> bool:
+    def __le__(self: NaturalNumber, obj: object) -> bool:
         return not self > obj
 
 
